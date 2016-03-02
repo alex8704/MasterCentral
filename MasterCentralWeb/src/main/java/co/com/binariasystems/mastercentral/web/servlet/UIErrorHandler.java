a53cd43077fdcdc135664849798eba87564da423
Alexander Castro O <alexander_8704@yahoo.com.co>
2016-03-01 21:05:34 -0500

    refactoring

---
+++ MasterCentralWeb/src/main/java/co/com/binariasystems/mastercentral/web/servlet/UIErrorHandler.java
@@ -0,0 +1,11 @@
+package co.com.binariasystems.mastercentral.web.servlet;
+
+import com.vaadin.server.ErrorEvent;
+import com.vaadin.server.ErrorHandler;
+
+public class UIErrorHandler implements ErrorHandler {
+	@Override public void error(ErrorEvent event) {
+		
+	}
+
+}
